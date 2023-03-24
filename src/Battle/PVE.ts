6976562d812d8c9469c0b90c3f169c6ae323d9ab
Nathália <nathaliamarciaandrade@gmail.com>
2023-03-24 19:48:48 -0300

    Iniciando requisito 11

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,21 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Monster from '../Monster';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  constructor(
+    private _player: Fighter, 
+    private _enemy: (Fighter | SimpleFighter | Monster)[],
+  ) {
+    super(_player);
+  }
+
+  fight(): number {
+    this._enemy.forEach((enemy) => {
+      this._player.attack(enemy);
+      enemy.attack(this._player);
+    });
+
+    return this._player.lifePoints === -1 ? -1 : 1;
+  }
+}
