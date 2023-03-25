74b5337898abbb291b96e48ca4f6dff354631e77
Nathália <nathaliamarciaandrade@gmail.com>
2023-03-24 21:07:41 -0300

    Ajustando requisito 10 e iniciando o 13

--- src/Battle/index.ts
+++ src/Battle/index.ts
@@ -1,6 +1,6 @@
 import Battle from './Battle';
-import PVE from './PVE';
+// import PVE from './PVE';
 import PVP from './PVP';
 
 export default Battle;
-export { PVP, PVE };
+export { PVP };
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import Battle, { PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('Nathália');
+const player2 = new Character('Maria');
+const player3 = new Character('Rond');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+
+// const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]) {
+  battles.map((battle) => battle.fight());
+}
+
+export { player1, player2, player3, monster1, monster2, pvp, runBattles };
+// export { pve };
