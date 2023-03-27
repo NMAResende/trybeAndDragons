8d32903fcc8648c73f49d881cc2827bd47ca859b
Nathália <nathaliamarciaandrade@gmail.com>
2023-03-27 14:39:13 -0300

    Organizando requisito 13

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const player3 = new Character('Rond');
 player1.levelUp();
 player1.levelUp();
 player1.levelUp();
+player1.levelUp();
 
 const monster1 = new Monster();
 const monster2 = new Dragon();
